6b2e1607811246cc68ce0fc6d7fb4ec96bffbbbf
Spartak <spartakkpopov@gmail.com>
2026-03-20 13:07:49 +0100

    just built gradle wrapper and added dockerfile

--- LastMeterBackend/src/main/java/org/example/lastmeterbackend/LastMeterBackendApplication.java
+++ LastMeterBackend/src/main/java/org/example/lastmeterbackend/LastMeterBackendApplication.java
@@ -2,9 +2,17 @@ package org.example.lastmeterbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LastMeterBackendApplication {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LastMeterBackendApplication.class, args);

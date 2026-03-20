c438513937465a0dee38c34af47b7bdefae30bdc
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2026-03-20 09:27:55 +0100

    Initial commit

---
+++ LastMeterBackend/src/main/java/org/example/lastmeterbackend/LastMeterBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.lastmeterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastMeterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastMeterBackendApplication.class, args);
+    }
+
+}
---
+++ LastMeterBackend/src/test/java/org/example/lastmeterbackend/LastMeterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lastmeterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastMeterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

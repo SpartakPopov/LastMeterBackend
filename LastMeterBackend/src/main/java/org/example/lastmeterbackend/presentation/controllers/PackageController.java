7152c5955eb80f54b4086f991e007f859409c0aa
Кристиян Янков <39673325+ChrisY60@users.noreply.github.com>
2026-04-13 14:09:37 +0200

    fixed import error

--- LastMeterBackend/src/main/java/org/example/lastmeterbackend/presentation/controllers/PackageController.java
+++ LastMeterBackend/src/main/java/org/example/lastmeterbackend/presentation/controllers/PackageController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import org.example.lastmeterbackend.presentation.mappers.PackageResponseDtoMapper;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
